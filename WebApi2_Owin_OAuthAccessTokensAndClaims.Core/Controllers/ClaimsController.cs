d20b04b2c064f7917b22212d44130a559c942ba6
Paul <paul.j.millar.1971@gmail.com>
2016-07-27 14:17:42 +0100

    Added Roles and Claims Controllers

---
+++ WebApi2_Owin_OAuthAccessTokensAndClaims.Core/Controllers/ClaimsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Web.Http;
+using WebApi2_Owin_OAuthAccessTokensAndClaims.Core.Controllers.Abstract;
+
+namespace WebApi2_Owin_OAuthAccessTokensAndClaims.Core.Controllers
+{
+    [RoutePrefix("api/claims")]
+    public class ClaimsController : BaseApiController
+    {
+        /// <summary>
+        /// Responsible for unpacking claims in the JWT and returning them
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult GetClaims()
+        {
+            var identity = User.Identity as ClaimsIdentity;
+
+            var claims = from c in identity.Claims
+                         select new
+                         {
+                             subject = c.Subject.Name,
+                             type = c.Type,
+                             value = c.Value
+                         };
+
+            return Ok(claims);
+        }
+    }
+}
---
+++ WebApi2_Owin_OAuthAccessTokensAndClaims.Core/Controllers/RolesController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi2_Owin_OAuthAccessTokensAndClaims.Core.Controllers
+{
+    public class RolesController : ApiController
+    {
+    }
+}
